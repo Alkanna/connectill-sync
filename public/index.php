406700c6264edce3235846fdf657f40269cb919d
Khiman Louer <khiman.louer@madagence.com>
2020-06-27 22:49:55 +0200

    Removed unnecessary php settings

--- public/index.php
+++ public/index.php
@@ -5,14 +5,8 @@ use Whoops\Run;
 
 require '../vendor/autoload.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $whoops = new Run;
 $whoops->pushHandler(new PrettyPageHandler);
 $whoops->register();
-
-echo 'bitch';
 
 throw new RuntimeException('Oh Snap!');
